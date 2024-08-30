fe25255cf09ad9f093f358fc4476b315a6811657
Lucas pacheco <lucaspacheco1997@hotmail.com>
2024-08-29 21:57:13 -0300

    classe#5

--- projetoIntegradorApp/models.py
+++ projetoIntegradorApp/models.py
@@ -1,3 +1,7 @@
 from django.db import models
-
-# Create your models here.
+class AnimaisAdocao(models.Model):
+        nome = models.CharField(max_length=50)
+        raca = models.CharField(max_length=50)
+        condicaoSaude = models.CharField(max_length=50)
+        imagem = models.ImageField(upload_to='')
+        
