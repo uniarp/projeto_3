d981725520f64247a627a93bb610eccf8803ab06
LuisOtavio7 <luispassos223@gmail.com>
2024-08-29 22:00:52 -0300

    Denuncia #2

--- projetoIntegradorApp/models.py
+++ projetoIntegradorApp/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Denuncia(models.Model):
+    endereco = models.CharField(max_length=255)
+    observacao = models.TextField()
+    imagem = models.ImageField(upload_to='')
