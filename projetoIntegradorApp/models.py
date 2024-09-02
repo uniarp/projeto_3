d106ff041afa7983af514138eabbcf7b1e71d5b3
LuisOtavio7 <luispassos223@gmail.com>
2024-09-02 19:57:08 -0300

    Denuncia #2

--- projetoIntegradorApp/models.py
+++ projetoIntegradorApp/models.py
@@ -11,3 +11,8 @@ class CadastroAnimal(models.Model):
     nome = models.CharField(max_length=255)
     especie = models.CharField(max_length=100)
     cor = models.CharField(max_length=50)
+    
+class Denuncia(models.Model):
+    endereco = models.CharField(max_length=255)
+    observacao = models.TextField()
+    imagem = models.TextField()
