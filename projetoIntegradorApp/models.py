dec03f97178ad3ed08051dbf3309cd5d7353ef1d
Lucas pacheco <lucaspacheco1997@hotmail.com>
2024-09-12 20:32:08 -0300

    classe#3

--- projetoIntegradorApp/models.py
+++ projetoIntegradorApp/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class AnimaisAdocao(models.Model):
-        nome = models.CharField(max_length=50)
-        raca = models.CharField(max_length=50)
-        condicaoSaude = models.CharField(max_length=50)
-        imagem = models.ImageField(upload_to='')
+    nome = models.CharField(max_length=50)
+    raca = models.CharField(max_length=50)
+    condicaoSaude = models.CharField(max_length=50)
+    imagem = models.ImageField(upload_to='')
 
 class CadastroAnimal(models.Model):
     animal_id = models.IntegerField(max_length=20)
