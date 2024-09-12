fcfdde6bfb83c9be496b752029cf24ac93cce244
David Ruza <david_99ruza@outlook.com>
2024-09-12 19:52:07 -0300

    CadastroDeTutor #4

--- projetoIntegradorApp/models.py
+++ projetoIntegradorApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class CadastroTutor(models.Model):
+    nome = models.CharField(max_length=100)
+    cpf = models.CharField(max_length=14)
+    email = models.EmailField(max_length=100)
+    endereco = models.CharField(max_length=255)
+    numero = models.CharField(max_length=15)
+    financas = models.DecimalField(max_digits=10, decimal_places=2)
